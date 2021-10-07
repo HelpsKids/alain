30a6221857f577b75b1993dbfa20532230cf3b5e
HelpsKids <you@example.com>
2021-10-07 21:58:08 +0200

    Premier commit

---
+++ mail/donate.php
@@ -0,0 +1,19 @@
+<?php
+if(empty($_POST['name']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+
+
+$to = "helpsforchildren@gmail.com"; // Change this email to your //
+
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?>
